957e26b8510b945166778c5d9196e622d9a0acab
unknown <chlalrud0702@naver.com>
2019-03-11 18:14:15 +0900

    0311

---
+++ sdedu/exercise/0307.java
@@ -0,0 +1,7 @@
+public class Hello07{
+  public static void main(String args[]){
+    System.out.println("홍홍홍");
+    System.out.print("길길길");
+    System.out.println("동동동");
+  }
+}
---
+++ sdedu/exercise/ProfilePrint.java
@@ -0,0 +1,13 @@
+class ProfilePrint{
+  byte age;
+  String name;
+  boolean isMarried;
+
+  public void setAge(byte age){
+    this.age = age;
+  }
+
+  public void getAge(){
+    return age;
+  }
+}
